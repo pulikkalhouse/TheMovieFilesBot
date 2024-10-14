eb1618a510ff136f2424debef9c79b4ccf60515c
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2024-10-14 10:19:05 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1576,7 +1576,7 @@ async def auto_filter(client, msg, spoll=False , pm_mode = False):
                 try:
                     await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024] + links + js_ads, reply_markup=InlineKeyboardMarkup(btn))
                 except pyrogram.errors.ButtonUserPrivacyRestricted:
-    print("User's privacy settings restrict button creation.")
+                    print("User's privacy settings restrict button creation.")
     await message.reply_text("Sorry, your privacy settings do not allow interaction with this bot.")                    
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
